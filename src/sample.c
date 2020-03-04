c722d82a8de9c0d0212b4a9c8b2bb8a1ab34f89e
AmirHossein Sojoodi <amir.sojoodi@queensu.ca>
2020-03-03 20:54:31 -0500

    Develop: a sample OpenMP code.

---
+++ src/sample.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include<omp.h>
+#include<stdio.h>
+
+#define SIZE 10000
+
+int main(){
+
+	int *array;
+	int size = SIZE;
+	array = (int *)malloc(size * sizeof(int));
+
+	#pragma omp parallel
+	for(int i = 0; i < SIZE; i++){
+		array[i] = i;
+	}
+
+	// A simple validity test
+	printf("Array[%d] = %d", size - 1, array[size - 1]);
+
+	free(array);
+
+	return 0;
+}
+
