d0f66f76d34e0cb184e820a0b668eb644c3613ea
amirsojoodi <amir.sojoodi@gmail.com>
2020-04-07 12:28:41 -0400

    Dev: implement 2 rules to resolve all the mallocs and frees

---
+++ src/output.c
@@ -0,0 +1,24 @@
+#*******************************************************************
+# sample.c
+# 
+# A simple array initialization in OpenMP
+#*******************************************************************
+#include<stdlib.h>
+#include<omp.h>
+#include<stdio.h>
+#define SIZE 10000
+#define foo 10
+
+int main () {
+    int *array;
+    int size = SIZE;
+    array = (int *) cudaMallocManaged (size * sizeof (int));
+#pragma omp parallel    
+    for (int i = 0; i < size; i++) {
+        array[i] = i;
+    }
+    printf ("Array[%d] = %d", size - 1, array [size - 1]);
+    cudaFree (array);
+    return 0;
+}
+
