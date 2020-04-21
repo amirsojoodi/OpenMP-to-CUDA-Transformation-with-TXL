defd20514eb7401488250925792a1664fbab882c
Nicolas Merz <nicolasmerz19@gmail.com>
2020-04-21 14:46:34 -0400

    Extract array names from for loop

---
+++ src/sample2.c
@@ -0,0 +1,35 @@
+#*******************************************************************
+# sample.c
+# 
+# A simple array initialization in OpenMP
+#*******************************************************************
+
+#include<stdlib.h>
+#include<omp.h>
+#include<stdio.h>
+
+#define SIZE 10000
+
+int main(){
+
+	int *array;
+	int *array2;
+	int size = SIZE;
+	array = (int *)malloc(size * sizeof(int));
+	array2 = (int *)malloc(size * sizeof(int));
+
+	#pragma omp parallel
+	for(int i = 0; i < size; i++){
+		array[i] = i;
+        array2[i] = i+1;
+	}
+
+	// A simple validity test
+	printf("Array[%d] = %d", size - 1, array[size - 1]);
+
+	free(array);
+    free(array2);
+
+	return 0;
+}
+
